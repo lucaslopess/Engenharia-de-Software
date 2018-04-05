c6d37fab372da9f8b328c6403013fd7c11d7a1aa
lucas.fernando <lucas.fernando@LAB5-DCX-27.dcx.ufpb.br>
2018-04-05 10:46:17 -0300

    First!

---
+++ src/Lucas.java
@@ -0,0 +1,10 @@
+
+public class Lucas {
+
+	public static void main(String[] args) {
+		// TODO Stub de método gerado automaticamente
+		System.out.println("Só fazendo uns teste Rogerinho!");
+
+	}
+
+}
